73e9c165c1619265f729f888f2a89f5a76ffaffa
mamfyou <mamf20021381@gmail.com>
2024-05-28 12:15:30 +0330

    adding secret key to settings.py

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: keep the secret key used in production secret!
 DEBUG = bool(int(config('DEBUG')))
